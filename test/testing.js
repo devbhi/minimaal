9b5b3de282a759ee35650bafab9d5c73dd4a29fd
abhii371 <abhisheknale1709@gmail.com>
2024-06-07 23:25:12 +0530

    route router layer request response added

--- test/testing.js
+++ test/testing.js
@@ -1,17 +1,29 @@
 const minimal = require("../lib/minimal");
+const bodyParser = require("../lib/bodyParser");
 const app = minimal();
 
-// app.use((req, res, next) => {
-//   console.log(`${req.method} ${req.url}`);
-//   next();
-// });
-
-//////
+app.use(bodyParser);
 
 app.get("/", (req, res) => {
-  res.end("Hello World!");
+  res.send("Hello World!");
+});
+
+app.post("/user", (req, res) => {
+  res.send("Hello World!)))))))");
+});
+
+app.put("/user/:id", (req, res) => {
+  res.json({ message: `User ${req.params.id} Updated`, data: req.body });
+});
+
+app.delete("/user/:id", (req, res) => {
+  res.status(204).send();
+});
+
+app.patch("/user/:id", (req, res) => {
+  res.json({ message: `User ${req.params.id} Patched`, data: req.body });
 });
 
 app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+  console.log("Server is running on port $4000");
 });
