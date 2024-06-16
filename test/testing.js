906ec8f01bb4e069aed41e42e15828aabec209d3
abhii371 <abhisheknale1709@gmail.com>
2024-06-16 20:46:28 +0530

    version 1.0.0 completed, testing remaining

--- test/testing.js
+++ test/testing.js
@@ -20,26 +20,62 @@ app.use(minimal.urlencoded({ extended: true }));
 //   res.send("Hello World!)))))))");
 // });
 
-app.get("/", User.where("name").equals("John Doe").where("age").gt(15));
+app.get("/", User.find());
 
-app.post("/user", (req, res) => {
-  console.log(req);
-  let body = req.body;
-  res.send(body);
-});
+// app.post("/user", (req, res) => {
+//   // console.log(req);
+//   let body = req.body;
+//   res.send(body);
+// });
+
+// const req = {
+//   body: {
+//     name: "abhiLLLL",
+//     age: 22,
+//     email: "abhi@test.com",
+//   },
+// };
+
+// let a = () => {};
+// console.log(typeof a);
+
+// const a = User.create({
+//   name: "name",
+//   age: 1,
+//   email: "email",
+// });
+
+// console.log(typeof a);
+
+app.post(
+  "/user",
+  query((req) =>
+    User.create({
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+    })
+  )
+);
 
 // app.post(
 //   "/user",
-//   User.create({
-//     name: "John Doe",
-//     age: 30,
-//     email: "john.doe@example.com",
-//   })
+//   query((req) => User.where("name").equals(req.body.name))
 // );
 
-app.put("/user/:id", (req, res) => {
-  res.json({ message: `User ${req.params.id} Updated`, data: req.body });
-});
+// app.post(
+//   "/getUser",
+//   // handleMongooseQuery((req) => User.where("name").equals(req.body.name))
+//   query((req) => User.where("name").equals(req.body.name))
+// );
+
+app.put(
+  "/user",
+  query((req) => User.where("name").gt(req.body.name))
+  //   (req, res) => {
+  //   res.json({ message: `User ${req.params.id} Updated`, data: req.body });
+  // }
+);
 
 app.delete("/user/:id", (req, res) => {
   res.status(204).send();
